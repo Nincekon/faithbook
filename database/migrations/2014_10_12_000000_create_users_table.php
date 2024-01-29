2ecefc2b9b557e472296571165ea11943f6e8c8b
Nincekon YORO Zandé <wilfriedyoro68@gmail.com>
2024-01-29 16:56:35 +0000

    📦 NEW: Add Models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,14 +8,16 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username', 16)->unique();
+            $table->string('firstname', 35)->nullable();
+            $table->string('lastname', 20)->nullable();
+            $table->string('cover_path', 1024)->nullable();
+            $table->string('avatar_path', 1024)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -26,10 +28,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
---
+++ database/migrations/2024_01_29_102149_create_post_attachments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_attachments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained();
+            $table->string('name');
+            $table->string('mime', 25);
+            $table->integer('size');
+            $table->string('url', 1024);
+            $table->string('path');
+            $table->foreignId('created_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_attachments');
+    }
+};
